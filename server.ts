7927cb3ad48400868c3a741fc64a7cd0f78e2175
Suwandre <suwandresukijat@gmail.com>
2024-10-16 23:37:22 +0200

    chore: server.ts initialized

--- server.ts
+++ server.ts
@@ -1,9 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import session from 'express-session';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT!;
-const mongoUri = process.env.MONGODB_URI!;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// temporarily allowing all cors requests
+app.use(cors());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.listen(port, async () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+export default app;
